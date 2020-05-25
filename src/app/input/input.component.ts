0ac4ac31dc5d237046620d2b748c0d4ba76685a7
ny.iuriii@gmail.com <ny.iuriii@gmail.com>
2020-05-25 18:01:45 +0200

    ended on (super.setValue)

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -7,11 +7,17 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
-  @Input() control: FormControl
+  @Input() control: FormControl;
+  @Input() label: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  showError(){
+    const {dirty, touched, errors} = this.control
+    return dirty && touched && errors
+    
   }
 
 }
